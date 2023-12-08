7b5761b3f9321559568abb1255b001278cc794d4
mateidragony <matei.cloteaux@gmail.com>
2023-12-08 12:21:19 -0500

    Add Q8

---
+++ q8.java
@@ -0,0 +1,130 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class q8 {
+
+    // Reason for sourcing: I was lazy and didn't want to write this math code myself...
+    private static long gcd(long a, long b){ // Source: Jeffrey Hantin Stack Overflow https://stackoverflow.com/questions/4201860/how-to-find-gcd-lcm-on-a-set-of-numbers
+        while (b > 0){
+            long temp = b;
+            b = a % b; // % is remainder
+            a = temp;
+        }
+        return a;
+    }
+
+    private static long gcd(long[] input){ // Source: Jeffrey Hantin Stack Overflow https://stackoverflow.com/questions/4201860/how-to-find-gcd-lcm-on-a-set-of-numbers
+        long result = input[0];
+        for(int i = 1; i < input.length; i++) result = gcd(result, input[i]);
+        return result;
+    }
+
+    private static long lcm(long a, long b){ // Source: Jeffrey Hantin Stack Overflow https://stackoverflow.com/questions/4201860/how-to-find-gcd-lcm-on-a-set-of-numbers
+        return a * (b / gcd(a, b));
+    }
+
+    private static long lcm(long[] input){ // Source: Jeffrey Hantin Stack Overflow https://stackoverflow.com/questions/4201860/how-to-find-gcd-lcm-on-a-set-of-numbers
+        long result = input[0];
+        for(int i = 1; i < input.length; i++) result = lcm(result, input[i]);
+        return result;
+    }
+
+
+
+    public static boolean allEndInZ(List<String> nodes){
+        for(String node : nodes) if(node.charAt(2) != 'Z') return false;
+        return true;
+    }
+
+    public static boolean allTrue(boolean[] bools){
+        for(boolean bool: bools) if(!bool) return false;
+        return true;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner s = new Scanner(new File("in.txt"));
+        List<String> lines = new ArrayList<>();
+        while(s.hasNextLine()){
+            lines.add(s.nextLine());
+        }
+
+        String instructions = lines.get(0);
+
+        HashMap<String, String> rightPath = new HashMap<>();
+        HashMap<String, String> leftPath = new HashMap<>();
+
+        List<String> curNodes = new ArrayList<>();
+
+        for(int i=2; i<lines.size(); ++i){
+            String line = lines.get(i);
+            String[] nodeAndDest = line.split(" = ");
+            String[] leftRightDest = nodeAndDest[1].substring(1, nodeAndDest[1].length()-1).split(", ");
+
+            leftPath.put(nodeAndDest[0], leftRightDest[0]);
+            rightPath.put(nodeAndDest[0], leftRightDest[1]);
+
+            if(nodeAndDest[0].charAt(2) == 'A') curNodes.add(nodeAndDest[0]);
+        }
+
+        // Part 1
+        String curNode = "AAA";
+        int curInstIdx = 0;
+        int numMoves = 0;
+        while(!curNode.equals("ZZZ")){
+            if(instructions.charAt(curInstIdx++) == 'R'){
+                curNode = rightPath.get(curNode);
+            } else {
+                curNode = leftPath.get(curNode);
+            }
+
+            if(curInstIdx >= instructions.length()) curInstIdx = 0;
+
+            numMoves++;
+        }
+
+
+        List<Set<String>> curPaths = new ArrayList<>();
+        long[] curPathCosts = new long[curNodes.size()];
+        boolean[] finishedPath = new boolean[curNodes.size()];
+        for(String node : curNodes){
+            Set<String> path = new HashSet<>();
+            path.add(node);
+            curPaths.add(path);
+        }
+
+        // Part 2
+        curInstIdx = 0;
+        while(!allTrue(finishedPath)){
+            for(int i=0; i<curNodes.size(); ++i){
+                HashMap<String, String> mapPathToTake;
+
+                if(instructions.charAt(curInstIdx) == 'R'){
+                    mapPathToTake = rightPath;
+                } else {
+                    mapPathToTake = leftPath;
+                }
+
+                curNode = curNodes.remove(i);
+                String nextNode = mapPathToTake.get(curNode);  
+                curNodes.add(i, nextNode);
+
+                curPaths.get(i).add(curNode);
+
+                if(curNode.charAt(2) == 'Z' && curPaths.get(i).contains(nextNode)){ // Path loop
+                    finishedPath[i] = true;
+                }
+
+                if(!finishedPath[i]) curPathCosts[i]++;
+
+            }
+
+            curInstIdx++;
+
+            if(curInstIdx >= instructions.length()) curInstIdx = 0;
+        }
+
+        System.out.println("Part 1: "+numMoves);
+        System.out.println("Part 2: " + lcm(curPathCosts));
+    }
+}
--- templateCodes/template.c
+++ templateCodes/template.c
@@ -6,23 +6,89 @@
 
 #define unsafe_op(e) {fprintf(stderr, "Error with %s\n", e); exit(EXIT_FAILURE);}
 
-int main(void){
+#define SPLIT_BUFF 10
+#define BUFFER 1024
 
-    char *file_name = "../in.txt";
-
+char *read_file(const char *file_name, int *buf_sz){
     int fd;
-    if((fd = open(file_name, O_RDONLY)) < 0) unsafe_op("open");
+    if((fd = open(file_name, O_RDONLY)) == -1) unsafe_op("open");
 
     struct stat sb;
-    stat(file_name, &sb);
-    intmax_t buf_sz = (intmax_t)sb.st_size;
+    if(fstat(fd, &sb) == -1) unsafe_op("fstat");
+    *buf_sz = (intmax_t)sb.st_size;
 
-    char *buf = malloc(buf_sz);
-    read(fd, buf, buf_sz);
+    char *buf; if((buf = malloc(*buf_sz)) == NULL) unsafe_op("malloc");
+    read(fd, buf, *buf_sz);
 
-    for(int i=0; i<buf_sz; ++i) printf("%c", buf[i]);
+    return buf;
+}
 
-    printf("\nbuf_sz: %d\n", buf_sz);
+char **get_lines(char *buf, int buf_sz, int *num_lines){
+    int line_buf_sz = BUFFER;
+    char **lines; if((lines = malloc(BUFFER * sizeof(char*))) == NULL) unsafe_op("malloc");
+    *num_lines = 0;
+
+    int cur_wrd_idx = 0;
+    int cur_wrd_buf_sz = BUFFER;
+    char *cur_wrd; if((cur_wrd = malloc(BUFFER * sizeof(char))) == NULL) unsafe_op("malloc");
+
+    for(int i=0; i<buf_sz; ++i){
+        if(buf[i] != '\n'){ // No new line
+            cur_wrd[cur_wrd_idx++] = buf[i];
+            if(cur_wrd_idx >= cur_wrd_buf_sz){ // prolly wont happen :P
+                cur_wrd_buf_sz += BUFFER;
+                if((cur_wrd = realloc(cur_wrd, cur_wrd_buf_sz)) == NULL) unsafe_op("realloc");
+            }
+        } else { // new word
+            cur_wrd[cur_wrd_idx] = '\0';
+            lines[(*num_lines)++] = cur_wrd;
+
+            if(num_lines >= line_buf_sz){
+                line_buf_sz += BUFFER;
+                if((cur_wrd = realloc(lines, line_buf_sz)) == NULL) unsafe_op("realloc");
+            }
+
+            cur_wrd_idx = 0;
+            if((cur_wrd = malloc(BUFFER * sizeof(char))) == NULL) unsafe_op("malloc");
+        }
+    }
+
+    cur_wrd[cur_wrd_idx] = '\0';
+    lines[(*num_lines)++] = cur_wrd;
+    cur_wrd_idx = 0;
+
+    free(buf); 
+    buf = NULL;
+
+    return lines;
+}
+
+
+char **split(char *str, const char *split_str){
+    char **splits; if((splits = malloc(SPLIT_BUFF * sizeof(char*))) == NULL) unsafe_op("malloc");
+    const int len = strlen(str);
+
+    int cur_wrd_idx = 0;
+    int cur_wrd_buf_sz = BUFFER;
+    char *cur_wrd; if((cur_wrd = malloc(BUFFER * sizeof(char))) == NULL) unsafe_op("malloc");
+
+    for(int i=0; i<)    
+}
+
+
+int main(int argc, char **argv){
+
+    char *file_name = "../intest.txt";
+    if(argc >= 2) file_name = argv[1];
+
+    int buf_sz;
+    int num_lines;
+
+    char *buf = read_file(file_name, &buf_sz);
+    char **lines = get_lines(buf, buf_sz, &num_lines);
+    
+
+    for(int i=0; i<num_lines; ++i) printf("%s\n", lines[i]);
 
     return 0;
 }

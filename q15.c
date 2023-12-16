5b514069343cc514c473d058e5b8d2d10887812c
mateidragony <matei.cloteaux@gmail.com>
2023-12-15 20:08:04 -0500

    Add Q15

--- q15.c
+++ q15.c
@@ -5,10 +5,35 @@
 #include <fcntl.h>
 #include <stdbool.h>
 
-#define unsafe_op(e) {fprintf(stdout, "Error with %s\n", e); exit(EXIT_FAILURE);}
+#define unsafe_op(e) {printf("Error with %s\n", e); exit(EXIT_FAILURE);}
 
 #define SPLIT_BUFF 10
 #define BUFFER 1024
+
+#define HASH_INSERT_SUCCESS 0
+#define HASH_INSERT_REPLACE 1 // For debugging
+#define HASH_INSERT_FAILURE -1
+#define HASH_REMOVE_SUCCESS 0
+#define HASH_REMOVE_NOOP 1 // For debugging
+#define HASH_REMOVE_FAILURE -1
+
+
+#define NUM_BOXES 256
+
+typedef struct lens {
+    char *label;
+    int focal_length;
+    struct lens *prev; // Doubly linked, fuck you its easier for me
+    struct lens *next;
+} lens_t;
+
+int make_lens(lens_t *lens, char *label, int focal_length, lens_t *prev, lens_t *next){
+    lens->label = label;
+    lens->focal_length = focal_length;
+    lens->prev = prev;
+    lens->next = next;
+    return EXIT_SUCCESS;
+}
 
 void set_char_arr_vals(char *arr, char val, int len){
     for(int i=0; i<len; ++i) arr[i] = val;
@@ -112,6 +137,7 @@ char **split(char *str, const char *split_str, int *num_splits){
                 set_char_arr_vals(split_buf, '\0', split_len); // We found a split so reset split_buf to 0
                 
                 cur_wrd[cur_wrd_idx] = '\0';
+
                 if(strlen(cur_wrd) > 0) splits[(*num_splits)++] = cur_wrd;
 
                 if(*num_splits >= splits_buf_sz){
@@ -146,9 +172,89 @@ char **split(char *str, const char *split_str, int *num_splits){
     return splits;
 }
 
+int hash_str(char *str){
+    int cur_val = 0;
+    int l = strlen(str);
+
+    for(int i=0; i<l; ++i){
+        cur_val = ((cur_val + (int)str[i]) * 17) % 256;
+    }
+
+    return cur_val;
+}
+
+int insert_or_replace_hash(lens_t **hash_map, char *key, int val){
+    int hash_idx = hash_str(key);
+    lens_t *cur = hash_map[hash_idx];
+
+    if(cur == NULL){ // Empty ll
+        lens_t *lens;
+        if((lens = malloc(sizeof(lens_t))) == NULL) unsafe_op("malloc");
+        make_lens(lens, key, val, NULL, NULL);
+        hash_map[hash_idx] = lens;
+        return HASH_INSERT_SUCCESS;
+    }
+
+    lens_t *prev;
+    // head is not null
+    while(cur != NULL){
+        if(strcmp(key, cur->label) == 0){ // replace
+            cur->focal_length = val;
+            return HASH_INSERT_REPLACE;
+        }
+
+        prev = cur;
+        cur = cur->next;
+    }
+
+    lens_t *lens;
+    if((lens = malloc(sizeof(lens_t))) == NULL) unsafe_op("malloc");
+    make_lens(lens, key, val, prev, NULL);
+    prev->next = lens;
+    return HASH_INSERT_SUCCESS;
+}
+
+int remove_hash(lens_t **hash_map, char *key){
+    int hash_idx = hash_str(key);
+    lens_t *cur = hash_map[hash_idx];
+
+    if(cur == NULL) return HASH_REMOVE_NOOP;
+
+    if(strcmp(key, cur->label) == 0){ // remove head
+        hash_map[hash_idx] = cur->next; 
+        if(cur->next != NULL) cur->next->prev = NULL;
+    }
+    cur = cur->next;
+
+    while(cur != NULL){
+        if(strcmp(key, cur->label) == 0){ // remove
+            if(cur->prev != NULL) cur->prev->next = cur->next;
+            if(cur->next != NULL) cur->next->prev = cur->prev;
+            free(cur);
+            return HASH_INSERT_SUCCESS;
+        }
+        cur = cur->next;
+    }
+
+    return HASH_REMOVE_NOOP;
+}
+
+void print_hash(lens_t **hash_map, FILE *fp){
+    fprintf(fp, "----------Hash Map----------\n");
+    for(int i=0; i<NUM_BOXES; ++i){
+        lens_t *cur = hash_map[i];
+        int count = 1;
+        
+        while(cur != NULL){
+            fprintf(fp, "Box: %d, Key: %s, Val: %d\n", i, cur->label, cur->focal_length);
+            cur = cur->next;
+        }
+    }
+    fprintf(fp, "----------------------------\n");
+}
+
 
 int main(int argc, char **argv){
-
     char *file_name = "../intest.txt";
     if(argc >= 2) file_name = argv[1];
 
@@ -158,13 +264,56 @@ int main(int argc, char **argv){
     char *buf = read_file(file_name, &buf_sz);
     char **lines = get_lines(buf, buf_sz, &num_lines);
 
-    for(int line_idx = 0; line_idx < num_lines; ++line_idx){
-        char *line = lines[line_idx];
+    int partOneSum = 0;
+    int partTwoSum = 0;
 
+    lens_t **hash_map;
+    if((hash_map = malloc(NUM_BOXES * sizeof(lens_t*))) == NULL) unsafe_op("malloc");
+    for(int i=0; i<NUM_BOXES; ++i) hash_map[i] = NULL; // Set all heads to NULL
+
+    for(int line_idx = 0; line_idx < num_lines; ++line_idx){ // Only one line for this one
+        char *line = lines[line_idx];
+        int num_splits;
+        char **splits = split(line, ",", &num_splits);
+
+        for(int i=0; i<num_splits; ++i){
+
+            partOneSum += hash_str(splits[i]); // Part one
+
+            // ------- Part Two -------
+            int str_len = strlen(splits[i]);
+
+            if (strchr(splits[i], '-') != NULL){ // remove operation
+                char *sub_str;
+                if((sub_str = malloc((str_len - 1) * sizeof(char))) == NULL) unsafe_op("malloc");
+                strncpy(sub_str, splits[i], str_len-1);
+                sub_str[str_len - 1] = '\0';
+
+                int ret = remove_hash(hash_map, sub_str);
+                if(ret == HASH_REMOVE_FAILURE) unsafe_op("remove_hash");
+            } else { // insert operation
+                int key_val_num;
+                char **key_val = split(splits[i], "=", &key_val_num);
+                if(key_val_num != 2) unsafe_op("split or some other thing higher up");
+
+                int ret = insert_or_replace_hash(hash_map, key_val[0], atoi(key_val[1]));
+                if(ret == HASH_INSERT_FAILURE) unsafe_op("insert_or_replace_hash");
+            }
+        }
+    }
+    
+    // go through hash map
+    for(int i=0; i<NUM_BOXES; ++i){
+        lens_t *cur = hash_map[i];
+        int count = 1;
+        while(cur != NULL){
+            partTwoSum += (i+1) * (count++) * cur->focal_length;
+            cur = cur->next;
+        }
     }
 
-    printf("Part One: \n");
-    printf("Part Two: \n");
+    printf("Part One: %d\n", partOneSum);
+    printf("Part Two: %d\n", partTwoSum);
 
     return 0;
 }
